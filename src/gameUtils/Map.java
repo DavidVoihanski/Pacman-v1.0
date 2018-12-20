4b541a810ac0d59a20ada6939b116ca72f977c30
Evgeny Vendrov <evgenyv94@gmail.com>
2018-12-20 21:52:48 +0200

    more docu and class sorting

--- src/gameUtils/Map.java
+++ src/gameUtils/Map.java
@@ -31,8 +31,8 @@ public class Map {
 	private double mapW;
 
 	/**
-	* basic default map constructor 
-	*/
+	 * basic default map constructor
+	 */
 	public Map() {
 		try {
 			this.topLeftP = new GpsCoord(32.10574, 35.20228, 0);
@@ -62,17 +62,15 @@ public class Map {
 
 	// custom constructor//
 	public Map(double lat1, double lon1, double lat2, double lon2, String imagePath) throws IOException {
-		//TO BE MADE 
+		// TO BE MADE
 	}
-	
-	
-	//************public methods************
-	
-	
+
+	// ************public methods************
+
 	// getters//
 	/**
 	 * 
-	 * @return the image of the map 
+	 * @return the image of the map
 	 * @throws IOException
 	 */
 	public BufferedImage getImage() throws IOException {
@@ -82,7 +80,8 @@ public class Map {
 	// setters
 	/**
 	 * setting the image of the map
-	 * @param inputImage the wanted image to set 
+	 * 
+	 * @param inputImage the wanted image to set
 	 * @throws IOException
 	 */
 	public void setImage(BufferedImage inputImage) throws IOException {
@@ -90,65 +89,83 @@ public class Map {
 	}
 
 	/**
-	 * adding a meter vector to the left upper pixel to determine the Gps Coord of the pixel
+	 * returning a Gps Coord based on a given pixel on given map
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
 	public GpsCoord pixel2Gps() throws IOException {
-		Point3D vectorMeter = calcDiffMeterVector();
+		Point3D vectorMeter = calcDiffMeterVector();// calculating the difference meter vector and adding it to the to
+													// left pixel after we normalize it by the ratio
 		return (new GpsCoord(this.topLeftP.add(vectorMeter)));
 	}
 
 	/**
+	 * converting gps coord to pixels in a given map (i.e map image)
 	 * 
 	 * @param gps
 	 * @return
 	 */
 	public Point3D gps2Pixel(GpsCoord gps) {
 		Point3D temp = new Point3D(gps.getLat(), gps.getLon(), gps.getAlt());
-		Point3D vector = topLeftP.vector3D(temp);
-		double hRatio = calcDymHRatio();
-		double wRatio = calcDymWRatio();
+		Point3D vector = topLeftP.vector3D(temp); // creating a meter vector between the top left Gps coord to the
+													// chosen point)
+		double hRatio = calcDymHRatio();// calculating height ratio of the resolution as for right now
+		double wRatio = calcDymWRatio();// calculating width ratio of the resolution as for right now
+		// taking the integer part of the division as we talk about pixels
 		double y = Math.ceil(vector.y() / wRatio);
 		double x = Math.ceil(-vector.x() / hRatio);
-		Point3D output = new Point3D(x, y, 0);
+		Point3D output = new Point3D(x, y, 0);// creating a new point and returning it - these are the pixels
 		return output;
 
 	}
 ////private function section///
 
+	// this method calculates the ratio of the screen right now based on the
+	// original image resolution
 	private void calcRatio() {
+		// getting both height and width of the map right now
 		double height = this.mapImage.getHeight();
 		double width = this.mapImage.getWidth();
+		// calculating the diagonal of the screen based on Pitgors theorem
 		double diagonal = Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2));
+		// calculating the distance between the top left and bottom right corners
 		double distance = this.topLeftP.distance2D(this.bottomRightP);
+		// returning the ratio as a division between the diagonal and the distance
 		this.ratio = (distance / diagonal);
 	}
 
+	// calculating changing - dynamic height ratio
 	private double calcDymHRatio() {
 		double dynHeight = MyFrame.height;
-		return this.mapH / dynHeight;
+		return this.mapH / dynHeight;// the division between maps height and the current guis height
 	}
 
+	// calculating changing - dynamic width ratio
 	private double calcDymWRatio() {
 		double dynWidth = MyFrame.width;
-		return this.mapW / dynWidth;
+		return this.mapW / dynWidth;// the division between maps width and the current guis width
 	}
 
+	// calculating both width and height ratio and returning them both as a 3D point
 	private Point3D calcDynamicRatio() {
 		double dynHeight = MyFrame.height;
 		double dynWidth = MyFrame.width;
 		return new Point3D(dynHeight / originalHeigth, dynWidth / originalWidth, 0);
 	}
 
+	// calculating the meter difference vector between the last clicked pixel and
+	// the top left corner, used to determine pixels GPS coord
 	private Point3D calcDiffMeterVector() throws IOException {
-		Point3D dynamicRatio = calcDynamicRatio();
-		Point3D clicked = MyFrame.lastClicked;
+		Point3D dynamicRatio = calcDynamicRatio();// calculating the new - updated ratio based on the size of the screen
+													// right now
+		Point3D clicked = MyFrame.lastClicked;// saving the last clicked pixel as a 3D point
 		Point3D vector = new Point3D((((-1) * (clicked.y() - topLeftPixelCoord.y())) * (ratio / dynamicRatio.x())),
-				(clicked.x() * (ratio / dynamicRatio.y())), 0);
+				(clicked.x() * (ratio / dynamicRatio.y())), 0);// calculating the actual vector and returning it
 		return vector;
 	}
-
+	
+	//initializing the map used to make constructor less "ugly"
 	private void initDefMap() throws IOException {
 		mapH = topLeftP.distance2D(bottomLeftP);
 		mapW = bottomLeftP.distance2D(bottomRightP);
